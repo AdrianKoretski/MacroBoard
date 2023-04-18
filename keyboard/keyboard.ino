016d17557f8f2874ff1a22a7d7f82affc264a136
Adrian Koretski <adrian.koretski@gmail.com>
2023-04-18 13:26:41 -0400

    Added logic to use basic stepper motor functionality.

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -1,19 +1,38 @@
 bool m_handshake_complete = false;
 
+int step_pin = 5;
+int direction_pin = 6;
+int enable_pin = 7;
+
+int step_time = 100;
+
 void setup()
 {
   Serial.begin(9600);
+
+  pinMode (step_pin, OUTPUT);
+  pinMode (direction_pin, OUTPUT);
+  pinMode (enable_pin, OUTPUT);
+  
+  digitalWrite(step_pin, LOW);
+  digitalWrite(direction_pin, LOW);
+  digitalWrite(enable_pin, HIGH);
 }
 
 void loop()
 {
-  handshake();
+  if (!m_handshake_complete)
+  {
+    handshake();
+  }
+  else
+  {
+    StepMotor();
+  }
 }
 
 void handshake()
 {
-  if (m_handshake_complete)
-    return;
   String incoming = Serial.readString();
   if (incoming == "STEPPER_TERMINAL\0")
   {
@@ -21,4 +40,13 @@ void handshake()
     Serial.write("STEPPER_CONTROLLER");
     Serial.write('\0');
   }
+}
+
+void StepMotor()
+{
+  digitalWrite(step_pin, HIGH);
+  delayMicroseconds(100);
+
+  digitalWrite(step_pin, LOW);
+  delayMicroseconds(100);
 }
